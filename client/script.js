29e903538e943ae4272fef4804d0ecdbcae1ae79
jesseamos <anietiejesse@gamil.com>
2023-02-17 13:00:19 +0100

    update the server url

--- client/script.js
+++ client/script.js
@@ -78,7 +78,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from server -> bot response
 
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://codex-9on0.onrender.com/", {
     method: "POST",
     headers: {
      'Content-Type': 'application/json',
